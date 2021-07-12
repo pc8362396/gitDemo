fea86adc9080c5c619896b500160c336cc6503a8
pc <1339314529@qq.com>
2021-07-12 21:23:29 +0800

    init

---
+++ src/main/java/com/demo/Test.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+/**
+ * @author ChengPeng
+ * @date 2021/7/12 - 21:14
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1st submit");
+    }
+}
